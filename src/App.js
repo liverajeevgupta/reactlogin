3c50981197c51f7e2a720e242b1b32f3676ca514
rajeev <test@test.com>
2021-08-14 08:51:27 +0530

    login registration html done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Login from "./containers/Login";
+import Register from "./containers/Register";
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
+import About from './components/Aboutus';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Error from './components/Error'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route component={Error} />
+          </Switch>  
+      <Footer />    
     </div>
   );
 }
